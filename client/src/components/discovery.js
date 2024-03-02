7ab2dc8d14edc03eea5b93e6da07860f7030f584
JahMeat <Jah0311@uw.edu>
2024-03-01 20:46:36 -0800

    some fixes

---
+++ client/src/components/discovery.js
@@ -0,0 +1,50 @@
+'use strict';
+import _ from 'lodash';
+
+function createColumn(props) {
+    let photoGallery = props.photoGallery.map((photo) => {
+        photo = <img src={photo.src} />;
+    })
+
+    return (<div class="flex-container column-box">{photoGallery}</div>);
+}
+
+export function createDiscoveryColumns(props) {
+    let photoGallery = props.photoGallery;
+    let columnArray = [];
+
+    let screenSize = props.windowWidth;
+    let numCol = 1;
+    if (screenSize > 367) {
+        numCol++;
+    }
+    if (screenSize > 663) {
+        numCol++;
+    }
+    if (screenSize > 800) {
+        numCol++;
+    }
+    if (screenSize > 1200) {
+        numCol++;
+    }
+    if (screenSize > 1500) {
+        numCol++;
+    }
+
+    if (numCol != 1) {
+        columnArray = _.chunk(photoGallery, Math(photoGallery.length / numCol));
+        for (let i = 0; i < numCol; i++) {
+            let currentColumn = columnArray[numCol];
+            let column = <createColumn photoGallery={currentColumn} />;
+            columnArray.push(column);
+        }
+    } else {
+        columnArray = <createColumn photoGallery={photoGallery} />;
+    }
+
+    return (
+        <div class="flex-container post-list">
+            {columnArray}
+        </div>
+    );
+}
