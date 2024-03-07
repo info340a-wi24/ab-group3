f8b89373a7ba10e3d9ec29c6209569af5c73e169
JahMeat <Jah0311@uw.edu>
2024-03-06 22:47:37 -0800

    how did we get here

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import './style.css'
+import App from './components/App';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
