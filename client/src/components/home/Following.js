3132ed45b7a3cf45ab175034083777958cfdce11
JahMeat <Jah0311@uw.edu>
2024-03-09 06:17:26 -0800

    following page mocked up

--- client/src/components/home/Following.js
+++ client/src/components/home/Following.js
@@ -1,9 +1,54 @@
 'use strict';
 import _ from 'lodash';
 
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { getDatabase, ref, onValue } from 'firebase/database';
+
+function CreateRestaurant(props) {
+    let restaurant = props.restaurant;
+    let src = restaurant.cover_pic;
+    let restaurantName = restaurant.restaurant_name;
+
+    return (
+        <figure className="restaurant-figure">
+            <a href="profile.html">
+                <img src={src} alt={restaurantName} />
+                <figcaption>{restaurantName}</figcaption>
+            </a>
+        </figure>
+    )
+}
 
 export function Following(props) {
+    let [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        let db = getDatabase();
+        let restaurantsRef = ref(db, "restaurants");
+
+        let unregisterFuntion = onValue(restaurantsRef, (snapshot) => {
+            let restaurantsValue = snapshot.val();
+            let restaurantsKeys = Object.keys(restaurantsValue);
+
+            let restaurantsArray = restaurantsKeys.map((key) => {
+                let singleRestaurant = { ...restaurantsValue[key] };
+                return singleRestaurant;
+            })
+            setRestaurants(restaurantsArray);
+        });
+
+        function cleanup() {
+            unregisterFuntion();
+        }
+        return cleanup;
+    }, [])
+
+    let restaurantsArray = [];
+    for (let i = 0; i < restaurants.length; i++) {
+        restaurantsArray.push(<CreateRestaurant restaurant={{...restaurants[i]}} />);
+    }
+
     return (
         <>
             <div className="flex-container home-option">
@@ -13,24 +58,7 @@ export function Following(props) {
                 <Link to="../recent" className="NomNom-button">Recent</Link>
             </div>
             <div className="flex-container post-list restaurant-list">
-                <figure className="restaurant-figure">
-                    <a href="profile.html">
-                        <img src={"https://i0.wp.com/www.drdavidludwig.com/wp-content/uploads/2017/01/1-RIS_6IbCLYv1X3bzYW1lmA.jpeg?w=800&ssl=1"} alt="hot sexy nikke girls" />
-                        <figcaption>Jah's Big Meat BBQ</figcaption>
-                    </a>
-                </figure>
-                <figure className="restaurant-figure">
-                    <a href="profile.html">
-                        <img src={"https://i0.wp.com/www.drdavidludwig.com/wp-content/uploads/2017/01/1-RIS_6IbCLYv1X3bzYW1lmA.jpeg?w=800&ssl=1"} alt="hot sexy nikke girls" />
-                        <figcaption>Jah's Big Meat BBQ</figcaption>
-                    </a>
-                </figure>
-                <figure className="restaurant-figure">
-                    <a href="profile.html">
-                        <img src={"https://i0.wp.com/www.drdavidludwig.com/wp-content/uploads/2017/01/1-RIS_6IbCLYv1X3bzYW1lmA.jpeg?w=800&ssl=1"} alt="hot sexy nikke girls" />
-                        <figcaption>Jah's Big Meat BBQ</figcaption>
-                    </a>
-                </figure>
+                {restaurantsArray}
             </div>
         </>
     );
