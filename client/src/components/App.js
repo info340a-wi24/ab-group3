20b6333aceb8750a1fb7d89413ca8c65e381e778
Thomas Lu <tlu2004@uw.edu>
2024-03-10 17:43:19 -0700

    login signup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,7 +14,32 @@ import { OpenPost } from './home/OpenPost'
 // Footer Component
 import { FooterDetail } from './Footer';
 
+import { Login } from './pages/login'
+import { auth } from "../index";
+import { ProtectedRoute } from './routes/ProtectedRoute';
+import { onAuthStateChanged } from "firebase/auth";
+
+
 function App() {
+  const [user, setUser] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setIsFetching(false);
+        return;
+      }
+
+      setUser(null);
+      setIsFetching(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (isFetching) {
+    return <h2>Loading...</h2>
+  }
   return (
     <div className="flex-container general-layout">
         <header>
@@ -22,16 +47,44 @@ function App() {
         </header>
         <main>
             <Routes>
-              <Route path="discover" element={<Discover />} >
-                <Route path="/discover/:postId" element={<OpenPost />} />
+              <Route index path = '/' element={<Login/>}></Route>
+              <Route path = '/discover'
+                element={
+                  <ProtectedRoute user={user}>
+                    <Discover></Discover>
+                  </ProtectedRoute>
+                } 
+              ><Route path='/discover/:postId'
+                element={
+                  <ProtectedRoute user={user}>
+                    <OpenPost></OpenPost>
+                  </ProtectedRoute>
+                } 
+              ></Route>
               </Route>
-              <Route path="following" element={<Following />} />
-              <Route path="saved" element={<Saved />} />
-              <Route path="recent" element={<Recent />} />
-              <Route path="create" element={<CreatePost />} />
-              <Route path="*" element={<Navigate to="discover" />} />
+              <Route path = '/following'
+                element={
+                  <ProtectedRoute user={user}>
+                    <Following></Following>
+                  </ProtectedRoute>
+                } 
+              ></Route>
+              <Route path = '/saved'
+                element={
+                  <ProtectedRoute user={user}>
+                    <Saved></Saved>
+                  </ProtectedRoute>
+                } 
+              ></Route>
+              <Route path = '/Recent'
+                element={
+                  <ProtectedRoute user={user}>
+                    <Recent></Recent>
+                  </ProtectedRoute>
+                } 
+              ></Route>
             </Routes>
-        </main>
+          </main>
         <footer>
             <FooterDetail />
         </footer>
